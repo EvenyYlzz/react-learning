2f9afb5ef4bbe31a2fd95e3c272e60c48669393d
EvenyYL <172463074@qq.com>
2020-10-13 23:41:48 +0800

    06部分react state

--- src/index.js
+++ src/index.js
@@ -3,8 +3,34 @@ import ReactDOM from 'react-dom';
 import './App.css'
 import './04style.css'
 
+//  React State 相当于 Vue 的 Data
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props)
+    //  状态（数据）——> view
+    this.state = {
+      time: new Date().toLocaleTimeString()
+    }
+
+    console.log(this.state.time)
+  }
+
+  render() {
+    return (
+      <h1>当前时间：{this.state.time}</h1>
+    )
+  }
+}
 
 ReactDOM.render(
-  <HelloWorld name='老陈' weather='下雨' />,
+  <Clock />,
   document.getElementById('root')
 )
+
+setInterval(() => {
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  )
+}, 1000)
