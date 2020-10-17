63453744e0376382734ceeed35ce5dfd2d6a165c
EvenyYL <172463074@qq.com>
2020-10-17 23:41:27 +0800

    14 生命周期一半

--- src/index.js
+++ src/index.js
@@ -1,46 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//  React 生命周期、钩子函数
+//  在这个生命周期中，有很多我们可以调用的事件
 
-let arr1 = [1,2,3,4,5,6]
+//  React生命周期的3个状态
+//  1.Mounting：将组件插入到DOM中
+//  2.Updating：将数组更新到DOM中
+//  3.Unmounting：将组件移除DOM中
 
-//  对数组直接进行循环，相当于for循环
-arr1.forEach((item,index,arr) => {
-//  无返回值
-//  用return 也没用
-})
+//  生命周期中的钩子函数（方法、事件）
+//  ComponentWillMount：组件将要渲染
+//  ComponentDidMount：组件渲染完毕
+//  ComponentWillReceiveProps：组件将要接受props数据
+//  ShouldComponentUpdate：在组件接收到新的状态state或props，判断是否更新，返回bool值
+//  ComponentWillUpdate：组件将要更新
+//  ComponentDidUpdate：组件已经更新
+//  ComponentWillUnmount：组件将要卸载
 
-let arr2 = ['香蕉','苹果','雪梨']
+class ComLife extends React.Component {
+  constructor(props){
+    super(props)
 
-//  对数组每一项进行加工，加工完成后，返回一个新的数组
-let res2 = arr2.map((item,index,arr) => {
-//  有返回值
-  let str = index + item + index
-  return str
-})
+    this.state = {
+      msg: 'HelloWorld'
+    }
 
-let arr3 = [1,2,3,4,5,6,7,8,9]
-//  filter，就是将想要的内容进行筛选，不要的内容去除，最终返回想要的内容数据
-let res3 = arr3.filter((item,index,arr) => {
-//  有返回值
-  if ( item % 2 ===0 ) {
-    //  通过返回true还是false进行选择，true就是想要，false就是去除
-    return true
-  } else {
-    return false
+    console.log('constructor构造函数')
   }
-})
 
-//  reduce，是对整个数组进行整合
-//  将数组每一项内容整合后，返回一个新的内容数据
-let arr4 = [1,2,3,4,5,6,7,8,9]
-let res4 = arr4.reduce((pre,next,index) => {
-  console.log('pre：',pre)
-  console.log('next：',next)
-  return pre + next
-}, 0)
+  componentWillMount() {
+    console.log('①componentWillMount组件将要渲染')
+  }
 
-console.log('res4：',res4)
+  componentDidMount() {
+    console.log('②componentDidMount组件渲染完毕')
+  }
 
-//  for...in...主要用于遍历对象，不适用于遍历数组
-//  for...of...可以用来遍历数组，类数组的对象，字符串（ES6）
+  componentWillReceiveProps() {
+    console.log('③componentWillReceiveProps组件将要接收新的状态state和props')
+  }
+
+  componentWillUpdate() {
+    console.log('④componentWillUpdate组件将要更新')
+  }
+
+  componentDidUpdate() {
+    console.log('⑤componentDidUpdate组件更新完毕')
+  }
+
+  componentWillUnmount() {
+    console.log('⑥componentWillUnmount组件移除')
+  }
+
+  render() {
+    console.log('render渲染函数')
+    return (
+      <div>
+        <h1>{this.state.msg}</h1>
+        <button onClick={this.changeMsg}>组件更新</button>
+      </div>
+    )
+  }
+
+  changeMsg=()=>{
+    this.setState({
+      msg: 'Hello 老师'
+    })
+  }
+}
+
+ReactDOM.render(
+  <ComLife></ComLife>,
+  document.getElementById('root')
+)
